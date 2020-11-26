7c2bb9ace0b7394be514b610d890b4f94fc68580
Lucas Mattera <lucasmattera97@gmail.com>
2020-11-26 19:01:21 -0300

    add navbar

---
+++ src/componentes/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "../css/Navbar.css";
+
+function Navbar(){
+
+    return(
+        
+        <div className= "container-block">
+            <div>
+                <header className= "header">
+                    <div sm={6}>
+                        <p className="titulo">TP FINAL CONSTRUCCION DE INTERFACES DE USUARIO </p> 
+                    </div>
+                </header>
+            </div>                
+         </div>
+    )
+}
+
+export default Navbar;
